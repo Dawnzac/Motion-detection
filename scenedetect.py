3d45e3564f50055a988283321e88721ffa7d118a
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-24 12:03:03 +0530

    Update scenedetect.py
    using some threads in action.

--- scenedetect.py
+++ scenedetect.py
@@ -1,55 +1,68 @@
-import cv2
+import cv2 as md1
+import cv2 as md2
 import datetime
 import ctypes
 import winsound
+from threading import Thread
 
 duration = 1000
 freq = 440
+def img1():
+    cap1 = md1.VideoCapture(0)
 
-#cap1 = cv2.VideoCapture(0)
+    mog = md1.createBackgroundSubtractorMOG2()
+    c_count = 0
 
-mog = cv2.createBackgroundSubtractorMOG2()
-
-cap2 = cv2.VideoCapture(0)
-c_count = 0
-
-while True:
-    #ret1, frame1 = cap1.read()
-    ret2, frame2 = cap2.read()
-    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    
-    fgmask = mog.apply(gray)
-    
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    fgmask = cv2.dilate(fgmask, kernel, iterations=1)
-    
-    contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    for contour in contours:
-        if cv2.contourArea(contour) < 1000:
-            continue
+    while True:
+        ret1, frame1 = cap1.read()
+        gray = md1.cvtColor(frame1, md1.COLOR_BGR2GRAY)
         
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        current_time = datetime.datetime.now()
-        print ("Detected Change at " , current_time )
-        c_count+= 1
-        print (c_count)
-        if c_count >= 2:
-            winsound.MessageBeep()
-            MB_SYSTEMMODAL = 0x00001000
-            ctypes.windll.user32.MessageBoxW(0, "Scene Change Detected", "Alert", 1)
+        fgmask = mog.apply(gray)
+        
+        kernel = md1.getStructuringElement(md1.MORPH_ELLIPSE, (5, 5))
+        fgmask = md1.erode(fgmask, kernel, iterations=1)
+        fgmask = md1.dilate(fgmask, kernel, iterations=1)
+        
+        contours, hierarchy = md1.findContours(fgmask, md1.RETR_EXTERNAL, md1.CHAIN_APPROX_SIMPLE)
+        
+        for contour in contours:
+            if md1.contourArea(contour) < 1000:
+                continue
+            
+            x, y, w, h = md1.boundingRect(contour)
+            md1.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            current_time = datetime.datetime.now()
+            print ("Detected Change at " , current_time )
+            c_count+= 1
+            print (c_count)
+            if c_count >= 2:
+                winsound.MessageBeep()
+                MB_SYSTEMMODAL = 0x00001000
+                ctypes.windll.user32.MessageBoxW(0, "Scene Change Detected", "Alert", 1)
+                break
+        
+        md1.imshow('Display Test', frame1)
+        #final = md1.hconcat([frame1, frame2])
+        if md1.waitKey(1000) == ord('q'):
+            break 
+    cap1.release()
+    md1.destroyAllWindows()
+    
+def img2():
+    cap2 = md2.VideoCapture(0)
+
+    while True:
+        ret2, frame2 = cap2.read()
+        
+        md2.imshow('Display Test Reference', frame2)
+        #final = md1.hconcat([frame1, frame2])
+        if md2.waitKey(1000) == ord('q'):
             break
-    
-    cv2.imshow('Display Test', frame2)
-    #cv2.imshow('Display Test Reference', frame1)
-    #final = cv2.hconcat([frame1, frame2])
-    #cv2.imshow("Display Test", final)
-    if cv2.waitKey(1000) == ord('q'):
-        break
-    
         
-#cap1.release()
-cap2.release()
-cv2.destroyAllWindows()
+    cap2.release()
+    md2.destroyAllWindows()
+
+#t1 = Thread(target=img1)
+#t1.start()
+t2 = Thread(target=img2)
+t2.start()
